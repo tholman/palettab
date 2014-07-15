2a5babb9d3e764b61f061ed044417f5a11d0b7f4
Tim <timothy.w.holman@gmail.com>
2014-07-15 02:03:14 -0400

    copy color when click panel

--- js/colorFonts.js
+++ js/colorFonts.js
@@ -1,7 +1,7 @@
 var colorFonts = (function() {
 
     'use strict';
-    var colorBars, reloadButton, reloadSvg;
+    var body, colorBars, reloadButton, reloadSvg, textField;
 
     var rotation = 0;
 
@@ -28,11 +28,13 @@ var colorFonts = (function() {
     // Initialize
     function init() {
 
-        // Cache elements
-        reloadButton = document.querySelector( '.reload' );
-        reloadSvg    = reloadButton.querySelector( 'svg' );
+        var i = 0;
+        for( i; i < colorBars.length; i++ ) {
+            colorBars[i].addEventListener( 'click', function( event ) { barClick( event ) } );
+        }
 
         // Bind events
+        reloadButton.addEventListener('click', function() { reloadClick() });
         reloadButton.addEventListener('click', function() { reloadClick() });
 
         setTimeout( function() {
@@ -47,6 +49,26 @@ var colorFonts = (function() {
         reloadColors();
     }
 
+    function barClick( event ) {
+        
+        var element = event.srcElement;
+        copyText( element.getAttribute('data-color') );
+        copySuccess( element );
+    }
+
+    // Copy text to clipboard.
+    function copyText( text ) {
+        textField.textContent = text;
+        document.body.appendChild( textField );
+        textField.select();
+        document.execCommand( 'copy' );
+        body.removeChild( textField );
+    }
+
+    function copySuccess( element ) {
+        console.log( "show success!" );
+    }
+
     function reloadColors() {
         assignColors( colors[ Math.floor( Math.random() * colors.length )].colors );
     }
@@ -55,14 +77,20 @@ var colorFonts = (function() {
 
         var i = 0;
         for( i; i < colorBars.length; i++ ) {
-            colorBars[i].style.backgroundColor = colorSet[i];
+            var bar = colorBars[i];
+            bar.style.backgroundColor = colorSet[i];
+            bar.setAttribute( 'data-color', colorSet[i] );
         }
     }
 
     function main() {
 
         // Caching
-        colorBars = document.querySelectorAll( '.colors .color' );
+        body         = document.body;
+        colorBars    = document.querySelectorAll( '.colors .color' );
+        reloadButton = document.querySelector( '.reload' );
+        reloadSvg    = reloadButton.querySelector( 'svg' );
+        textField    = document.createElement( 'textarea' );
 
         // Initialize
         init();
