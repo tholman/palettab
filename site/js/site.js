305ce42b66f48f330341ac364b76a4f3704c5bc1
Tim <timothy.w.holman@gmail.com>
2014-08-11 20:51:51 -0400

    share

--- site/js/site.js
+++ site/js/site.js
@@ -9,10 +9,24 @@
  *
  *********************************************/
 
-function app() {
+function App() {
 
     this.init = function() {
 
+   		// Load Twitter
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+        // Load Facebook
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
     }
 
 }
+
+var app = new App();
+app.init();
